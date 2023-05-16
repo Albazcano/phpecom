89bd54be1e186da9139b580dad61f2b38b5a9751
Albazc <albazcano@gmail.com>
2023-05-16 19:48:40 +0200

    connecting db

---
+++ config/dbcon.php
@@ -0,0 +1,20 @@
+<?php
+
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "phpecom";
+
+    //Creating DB connection
+    $con = mysqli_connect($host, $username, $password, $database);
+
+    //Check DB connection
+    if(!$con)
+    {
+            die("Connection Failed: ". mysqli_connect_error());
+    }
+    else 
+    {
+        echo "Connected successfully";
+    }
+?>

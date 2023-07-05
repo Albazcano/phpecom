d204aeb148540cee3041ccb8660ab03c1613445e
Albazc <albazcano@gmail.com>
2023-07-05 20:50:45 +0200

    modify footer sidebar and myfunctions add category

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -31,10 +31,16 @@
 
   <script>
 
-    <?php if(isset($_SESSION['message'])) { ?>
-        alertify.set('notifier','position', 'top-right');
-        alertify.success('<?= $_SESSION['message']; ?>');
-    <?php } ?>  
+    <?php 
+        if(isset($_SESSION['message'])) 
+        { 
+            ?>
+            alertify.set('notifier','position', 'top-right');
+            alertify.success('<?= $_SESSION['message']; ?>');
+            <?php 
+            unset($_SESSION['message']);
+        } 
+    ?>  
 
   </script>
 
--- functions/myfunctions.php
+++ functions/myfunctions.php
@@ -1,5 +1,14 @@
 <?php
 
+include('../config/dbcon.php');
+
+function getAll($table)
+{
+    global $con;
+    $query = "SELECT * from $table";
+    return $query_run = mysqli_query($con, $query);
+}
+
 function redirect ($url, $message) {
     $_SESSION['message'] = $message;
     header('Location: '.$url);
